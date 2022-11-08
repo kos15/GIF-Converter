c037c023f94e28f78fafaf1e90ae75d7d7f7a9b0
Koustubh Mishra <koustubhmishra@Koustubhs-MacBook-Air.local>
2022-11-08 16:57:07 +0530

    Initial build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { fetchFile, createFFmpeg } from "@ffmpeg/ffmpeg";
+
+const ffmpeg = createFFmpeg({ log: true });
 
 function App() {
-  return (
+  const [ready, setReady] = useState(false);  // To keep track wheter we successfull import ffmpeg lib
+  const [file, setFile] = useState(false);  // To keep track we successfull import our input file
+  const [gif, setGif] = useState({}); // to store our GIF file
+
+  /**
+   * @description To load FFMPEG library
+   */
+  const load = async () => {
+    await ffmpeg.load();
+    setReady(true);
+  };
+
+  /**
+   * @param {*} e 
+   * @description To upload an input video file
+   */
+  const uploadFile = (e) => {
+    document.getElementById('convert').setAttribute('display', 'none');
+    setFile(e.target.files[0]);
+  };
+
+  /**
+   * @description To convert uploaded video file to GIF
+   */
+  const converGif = async () => {
+    if (file) {
+      ffmpeg.FS("writeFile", "test.mp4", await fetchFile(file));
+      await ffmpeg.run(
+        "-i",
+        "test.mp4",
+        "-t",
+        "2.5",
+        "-ss",
+        "2.0",
+        "-f",
+        "gif",
+        "out.gif"
+      );
+      const data = ffmpeg.FS("readFile", "out.gif");
+      const url = URL.createObjectURL(
+        new Blob([data.buffer], { type: "image/gif" })
+      );
+      setGif(url);
+    }
+  };
+
+  /**
+   * @description To run load function on first load
+   */
+  useEffect(() => {
+    load();
+  }, []);
+
+  /**
+   * @description To enable convert button when file get uploaded
+   */
+  useEffect(() => {
+    document.getElementById('convert').setAttribute('display', 'block');
+  }, [file]);
+
+  return ready ? (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {file && (
+        <video controls width="250" src={URL.createObjectURL(file)}></video>
+      )}
+      <hr></hr>
+      <input type="file" onChange={uploadFile}></input>
+      <button id='convert' onClick={converGif}>Convert to GIF</button>
+      <div>
+        {gif ? (
+          <img src={gif}/>
+        ) : file ? (
+          <h1>Processing....</h1>
+        ) : (
+          <h1></h1>
+        )}
+      </div>
     </div>
+  ) : (
+    <h1> Loading...</h1>
   );
 }
 
